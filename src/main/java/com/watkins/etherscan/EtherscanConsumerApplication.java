13994d7b272de9f574eff532db1441f3d7d16dd9
Austin Watkins <acwatkins27@gmail.com>
2022-04-22 11:23:45 -0400

    Initial commit

---
+++ src/main/java/com/watkins/etherscan/EtherscanConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.watkins.etherscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtherscanConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtherscanConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watkins/etherscan/EtherscanConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watkins.etherscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtherscanConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
